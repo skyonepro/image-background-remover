2995c144dc342436cf53e1303c8f88a68859a1d4
root <root@localhost.localdomain>
2026-03-29 22:39:24 +0800

    fix: hardcode NEXT_PUBLIC_GOOGLE_CLIENT_ID for static export build

--- frontend-nextjs/next.config.js
+++ frontend-nextjs/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: '427782383186-ucgo3fgiusdbt9glrkv9qr205jmko1j8.apps.googleusercontent.com',
+  },
 }
 
 module.exports = nextConfig
